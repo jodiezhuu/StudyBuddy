efdc28fc0921aec069fc077dab7881386be0a1d9
jodiezhuu <jodiezhu12@gmail.com>
2022-07-28 13:13:57 -0400

    card screen

--- productivitytracker/navigation/screens/DetailsScreen.js
+++ productivitytracker/navigation/screens/DetailsScreen.js
@@ -13,28 +13,12 @@ export default function DetailsScreen({}) {
           title="Total Number of Productive Hours"
           subtitle={getTotalTime()}
         />
-        <Card.Content>
-          <Paragraph>hi</Paragraph>
-        </Card.Content>
-        <Card.Cover source={require('./monsters/monster2.png')} />
-        <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions>
       </Card>
       <Card>
         <Card.Title
           title="Total Number of Monsters"
           subtitle={getTotalMonster()}
         />
-        <Card.Content>
-          <Paragraph>hi</Paragraph>
-        </Card.Content>
-        <Card.Cover source={require('./monsters/monster2.png')} />
-        <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions>
       </Card>
     </>
   );
